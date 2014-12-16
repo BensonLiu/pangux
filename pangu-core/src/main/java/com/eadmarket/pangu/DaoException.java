ca6a975888fae8a6e8d9e577f8379dee87326a6a
danny <liuyongpo@gmail.com>
2014-12-17 00:20:30 +0800

    submit DaoException

--- pangu-core/src/main/java/com/eadmarket/pangu/DaoException.java
+++ pangu-core/src/main/java/com/eadmarket/pangu/DaoException.java
@@ -4,14 +4,14 @@ package com.eadmarket.pangu;
  * @author liuyongpo@gmail.com
  */
 public class DaoException extends Exception {
-	
-	public DaoException(Throwable t) {
-		super(t);
-	}
-	
-	public DaoException(String message, Throwable t) {
-		super(message, t);
-	}
-	
-	private static final long serialVersionUID = 1L;
+
+  private static final long serialVersionUID = 1L;
+
+  public DaoException(Throwable t) {
+    super(t);
+  }
+
+  public DaoException(String message, Throwable t) {
+    super(message, t);
+  }
 }
